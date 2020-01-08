02ae8a3197387960bdb833cde7b06213d6bbfad7
kiran1415 <kiran1415git@gmail.com>
2020-01-08 21:24:07 +0530

    setitng.py update

--- khidki/settings.py
+++ khidki/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r#r*yp(-4o@$$7658gnf9$=%-y+mwayag4v+7py=_j1-3t)7xy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['159.89.92.44']
 
@@ -129,13 +129,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-] 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+#STATICFILES_DIRS = [
+  #  os.path.join(BASE_DIR,'static')
+#] 
+#STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 #managing media
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
+
