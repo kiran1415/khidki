5bd15b36f00398d1141c82b7d22f720e63d43965
kiran1415 <kiran1415git@gmail.com>
2020-01-24 11:21:59 +0530

    new small change

--- cart/models.py
+++ cart/models.py
@@ -8,8 +8,8 @@ from shop.models import  Product
 
 
 class CartItem(models.Model):
-    product = models.ManyToManyField(Product, blank=True)
-    ordered = models.BooleanField(default=False)
+    product = models.ForeignKey(Product , on_delete=models.SET_NULL , null=True)
+    date_added = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
@@ -21,6 +21,10 @@ class CartItem(models.Model):
     def get_total_item_price(self):
         return self.quantity * self.product.price
 
+    def get_add_to_cart_url(self):
+        return reverse("shop:add-to-cart", kwargs={
+            'slug': self.slug
+        })
 
 
 
