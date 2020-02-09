fdbccea990f3c250f87453d470de06c82578ce2a
kiran1415 <kiran1415git@gmail.com>
2020-02-09 23:54:45 +0530

    static dir

--- khidki/settings.py
+++ khidki/settings.py
@@ -161,9 +161,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ] 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+#STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 #managing media
