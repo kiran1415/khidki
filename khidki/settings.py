0cb7135db98299050de6350f0afc13dfe1abd873
kiran1415 <kiran1415git@gmail.com>
2020-02-24 15:21:22 +0530

    debug false

--- khidki/settings.py
+++ khidki/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r#r*yp(-4o@$$7658gnf9$=%-y+mwayag4v+7py=_j1-3t)7xy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['localhost','khidkistudio.com','159.65.173.101','127.0.0.1']
@@ -171,5 +171,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 #managing media
 
 MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/products/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
 
