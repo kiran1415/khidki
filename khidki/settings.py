488bb53892c34cbfe5dbd8a2036fe6bdce3e8598
kiran1415 <kiran1415git@gmail.com>
2020-02-27 16:08:37 +0530

    changes

--- khidki/settings.py
+++ khidki/settings.py
@@ -164,4 +164,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 #managing media
 
 MEDIA_URL = '/media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
